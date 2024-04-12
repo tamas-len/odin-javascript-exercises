6b7e23065b9133de3860a5592e9f3757e1480cab
Tamas <lengyelandtamas02@gmail.com>
2024-04-12 10:13:15 +0200

    solved ex 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if (num < 0)
+    {
+        return "ERROR"
+    }
+    else
+    {
+        let out = "";
+        for (let i = 0; i < num; i++)
+        {
+            out += word;
+        }
+        return out;
+    }
 };
 
 // Do not edit below this line
