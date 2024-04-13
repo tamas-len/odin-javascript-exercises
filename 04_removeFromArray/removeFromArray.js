e02a441e30a306d2567ad3eff901cec78f214037
Tamas <lengyelandtamas02@gmail.com>
2024-04-13 15:52:47 +0200

    still not working

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,16 @@
-const removeFromArray = function(input) {
+const removeFromArray = function(input, ...filt) {
     let out = [];
-    filt = Array.from(arguments);
-    filt.shift();
+    /*filt = Array.from(arguments);
+    filt.shift();*/
 
-    for (let i = 0; i <= filt.length; i++)
+    for (let i = 0; i = filt.length; i++)
     {
         out = input;
-        out = out.filter(number => number !== filt[i]);
+        out = out.filter(number => number == filt[i]);
     }
 
+    //return input.filter(number => !args.includes(number));
+    //out.splice(-1, 1);
     return out;
 };
 
