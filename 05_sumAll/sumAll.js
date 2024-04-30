48f8d792d7477ed97d02eac7986a0b4a37315b7b
Tamas <lengyelandtamas02@gmail.com>
2024-04-30 14:24:29 +0200

    solved 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function(one, two) {
+    out = 0;
+    if(one < 0 || two < 0)
+    {
+        return "ERROR"
+    }
 
+    if(typeof(one) != 'number' || typeof(two) != 'number')
+    {
+        return "ERROR"
+    }
+
+    if(one < two)
+    {
+        for (let i = one; i <= two; i++)
+        {   
+            out += i;
+        }
+
+    }
+    else
+    {
+        for (let i = two; i <= one; i++)
+        {
+            out += i;
+        }
+
+    }
+    return out
 };
 
 // Do not edit below this line
