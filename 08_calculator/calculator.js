7106f2dd56dc9ac02d33933bca372bf5f311462b
Tamas <lengyelandtamas02@gmail.com>
2024-06-14 19:24:07 +0200

    solved except factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,21 +1,21 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current, 1)
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  return Math.pow(num, power)
 };
 
 const factorial = function() {
