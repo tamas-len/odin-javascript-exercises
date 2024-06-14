e3fda9c96f9f67589b40a28c79619f3729813aad
Tamas <lengyelandtamas02@gmail.com>
2024-06-14 19:53:41 +0200

    reverses word

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function (tested) {
     //turns word into array
+    let testedArr = tested.split('').reverse();
+    return testedArr
 
     //filtereli a punctuationt meg a szokozoket meg a szamokat meg mindent egy masik arraybol
 
