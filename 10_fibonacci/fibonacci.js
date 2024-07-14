1b573ed243924071ccfc8cfe833bb0fea56d1494
Tamas <lengyelandtamas02@gmail.com>
2024-07-14 18:01:44 +0200

    makes fibonacci until set amout of length returns ith item

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fib = [1, 1];
 
+    for(let i = 1; i <= 26; i++)
+    {
+        fib.push(fib[fib.length-1] + fib[fib.length-2]);
+    }
+
+    return fib[num-1];
 };
 
 // Do not edit below this line
