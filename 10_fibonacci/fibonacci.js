5b88fefb4443cd5ca5068998e89d60dda617d874
Tamas <lengyelandtamas02@gmail.com>
2024-07-14 18:09:21 +0200

    sequence is always long enough + cleanup

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,22 +1,15 @@
 const fibonacci = function(num) {
     let fib = [1, 1];
 
-    for(let i = 1; i <= 26; i++)
+    for(let i = 1; i <= num; i++)
     {
         fib.push(fib[fib.length-1] + fib[fib.length-2]);
     }
 
-    if(num < 0)
-    {
-        return "OOPS"
-    }
+    if(num < 0) return "OOPS";
+    if(num == 0) return 0;
 
-    if(num == 0)
-        return 0;
-    else
-    {
-        return fib[num-1];   
-    }
+    return fib[num-1];
 };
 
 // Do not edit below this line
