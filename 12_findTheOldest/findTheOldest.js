f28a9c45034c73ffd044d75c2280698c8b85196a
Tamas <lengyelandtamas02@gmail.com>
2024-07-14 18:45:54 +0200

    doesnt work yet

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(ppl) {
+    let oldest = 0;
 
+    for(let i = 0; i < ppl.length; i++)
+    {
+        if(ppl[i].yearOfDeath - ppl[i].yearOfBirth > ppl[oldest].yearOfDeath - ppl[oldest].yearOfBirth)
+        {
+            oldest = i;
+        }
+    }
+
+    return ppl[oldest].name;
 };
 
 // Do not edit below this line
